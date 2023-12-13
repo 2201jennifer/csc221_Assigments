dc4d325508a2392a0efebe2deb6522d843a26455
2201jennifer <136124434+2201jennifer@users.noreply.github.com>
2023-12-13 04:11:08 -0500

    Add files via upload

---
+++ Assigment4:/src/Questions.java
@@ -0,0 +1,17 @@
+public class Questions {
+    private String questionText;
+    private String correctAnswer;
+
+    public Questions(String questionText, String correctAnswer) {
+        this.questionText = questionText;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+}
